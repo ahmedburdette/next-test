273ef981940aaa89e6a66cb7e8048f6c74ebda91
Ahmed Burdette <ahmedburdette@gmail.com>
2023-05-25 19:17:50 -0700

    new header text

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,17 +5,8 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center mt-8 border-b-2 pb-5 sm:px-20 px-2">
       <Link href="/" className="flex space-x-2">
-        <Image
-          alt="header logo"
-          title="cassette tape icon"
-          src="/cassette-tape-black1.png"
-          className="sm:w-12 sm:h-12 w-8 h-8"
-          width={32}
-          height={32}
-        />
         <h1 className="sm:text-5xl text-2xl font-bold ml-2 tracking-tight">
-          Pl<span className="text-rose-800">a</span>yl
-          <span className="text-rose-800">i</span>st
+          J<span>∩</span>kebox
         </h1>
       </Link>
       <a
